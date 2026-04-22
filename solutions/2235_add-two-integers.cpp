c2b2759f107c449f2d6574b5347841639c8ce0e0
Stitch <484967158@qq.com>
2026-04-22 20:43:58 +0800

    chore: add Add Two Integers

---
+++ solutions/2235_add-two-integers.cpp
@@ -0,0 +1,15 @@
+// LeetCode Solution: Add Two Integers
+// Runtime: 0 ms | Memory: 7.7 MB
+// Tags: Math
+// --------------------------------------------------
+// Personal Approach Notes:
+//   - Approach: [Solution strategy and key ideas]
+//   - Time Complexity: [O(...) - analyze]
+//   - Space Complexity: [O(...) - analyze]
+//   - Key Insights: [Observations and potential pitfalls]
+//
+// --------------------------------------------------
+
+int sum(int num1, int num2) {
+    return num1+num2;
+}
