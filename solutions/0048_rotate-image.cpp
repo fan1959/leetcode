e96deaadeb8775539ac40eb67dfb707a8c58ae0c
Stitch <484967158@qq.com>
2026-04-22 20:51:18 +0800

    chore: add Rotate Image

---
+++ solutions/0048_rotate-image.cpp
@@ -0,0 +1,25 @@
+// LeetCode Solution: Rotate Image
+// Runtime: 0 ms | Memory: 10.1 MB
+// Tags: Array, Math, Matrix
+// --------------------------------------------------
+// Personal Approach Notes:
+//   - Approach: [Solution strategy and key ideas]
+//   - Time Complexity: [O(...) - analyze]
+//   - Space Complexity: [O(...) - analyze]
+//   - Key Insights: [Observations and potential pitfalls]
+//
+// --------------------------------------------------
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for(int i=0;i<matrix.size();i++){
+            for(int j=i+1;j<matrix.size();j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<matrix.size();i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
